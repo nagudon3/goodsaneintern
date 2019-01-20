e84246db3602516f1573f0dd6c35051e230dd459
nagudon3 <mohdnazrulm9@gmail.com>
2019-01-20 19:44:26 +0800

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,37 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@extends("layouts.app")
+@section("content")
+<div class="container">
+@if(session()->has('message'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+<strong>Done !!! </strong>{{ session()->get('message') }}
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+@endif
+<div class="col-md-6">
+<h1>Todo List</h1>
+<form method="POST" action={{url('/task')}}>
+{{csrf_field()}}
+<div class="form-group">
+<input type="text" class="form-control" name="task" placeholder="Enter Task">
+</div>
+<div class="form-group">
+<button type="submit" class="btn btn-success">Add</button>
+</div>
+</form>
+<hr>
+<ol>
+@foreach($tasks as $task)
+<li><a href ={{url('/'.$task->id.'/complete')}}>{{ $task->task }}</a></li>
+@endforeach
+</ol>
+<h4>Completed</h4>
+<ol>
+@foreach($completed_tasks as $c_task)
+<li><a href ={{url('/'.$c_task->id.'/delete')}}>{{ $c_task->task }}</a></li>
+@endforeach
+</ol>
+</div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
+Route::get('/', "TaskController@index");
+Route::post("/task", "TaskController@store");
+Route::get("/{id}/complete", "TaskController@complete");
+Route::get("/{id}/delete", "TaskController@destroy");
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
